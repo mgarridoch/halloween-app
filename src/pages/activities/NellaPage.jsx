4645021bf493b16fc8d55c12d8a9f0be28747928
Valentina Silva <mgarridoch@uc.cl>
2025-10-31 20:05:05 -0300

    Create NellaPage.jsx

---
+++ src/pages/activities/NellaPage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function NellaPage() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}>
+      <h1 style={{ color: '#f97316', fontSize: '2.5rem', textAlign: 'center' }}>
+        ¡Es hora de jugar a<br/>que Nella nos tire un Freestyle!
+      </h1>
+    </div>
+  )
+}
+export default NellaPage
